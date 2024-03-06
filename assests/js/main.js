d216c88d82d6439c35e602191973e5d8dae3ca7a
Joao <joaocia@gmail.com>
2024-03-06 07:31:40 -0300

    Ajustes CSS

--- assests/js/main.js
+++ assests/js/main.js
@@ -33,6 +33,12 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById ('profile.skills.hardSkills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(skill => `<li> <img src="${skill.logo}" alt="${skill.name}" title ="${skill.name}"> </li>`).join('')
+}
 
 
 
@@ -41,5 +47,6 @@ function updateSoftSkills(profileData) {
     const profileData = await fetchProfileData()
     upDateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
     
 })()
