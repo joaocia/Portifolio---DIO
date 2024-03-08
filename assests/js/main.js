80263fdf0d2af963be887119c9d219d166d79d08
Joao <joaocia@gmail.com>
2024-03-08 09:56:15 -0300

    Preenchendo Dados Portfolio

--- assests/js/main.js
+++ assests/js/main.js
@@ -47,6 +47,19 @@ function updateLanguages(profileData) {
     .map(languages => `<li> ${languages}`).join('')
 }
 
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById ('profile.portfolio')
+
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}> ${project.name}</h3>
+                <a href= "${project.url}" target="_blank">${project.url}</a>
+            </li>  
+        `
+    }).join('')
+}
+
 
 (async () => {
 
@@ -55,5 +68,6 @@ function updateLanguages(profileData) {
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
+    updatePortfolio(profileData)
     
 })()
